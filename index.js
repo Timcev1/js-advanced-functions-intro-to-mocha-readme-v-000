18a8d772bcf7ff28abe5a8e237915a70d98f5214
Timothy Cevallos <timcev@gmail.com>
2017-10-12 21:57:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function theTruth(){
 function sayHey(){
   return 'Hey!'
 }
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(name){
+  return `I love ${name}`
+}
+
+function shouting(name){
+  return `${name.caps}`
+}
